56b8b1220338817a53394a249bf60c1f0a5142c2
Santiago G. Tanodi <santi9865@hotmail.com>
2023-03-10 20:54:02 -0300

    Implementación de Parte A

---
+++ Tanodi-DesafioFinal/Modelo/Cotizacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanodi_DesafioFinal.Modelo
+{
+    class Cotizacion
+    {
+        public int ID { get; private set; } //Número de identificación
+        public DateTime FechaYHora { get; private set; } //Fecha y hora de creación
+        public int CodigoVendedor { get; private set; } //Codigo del vendedor que genero esta cotización
+        public string Prenda { get; private set; }
+        public int Cantidad { get; private set; }
+        public float Resultado { get; private set; }
+    }
+}
---
+++ Tanodi-DesafioFinal/Modelo/Vendedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanodi_DesafioFinal.Modelo
+{
+    class Vendedor
+    {
+        public string Nombre { get; private set; }
+        public string Apellido { get; private set; }
+        public int Codigo { get; private set; } //Código identificatorio del vendedor
+
+        public List<Cotizacion> HistorialCotizaciones { get; private set; }
+    }
+}
